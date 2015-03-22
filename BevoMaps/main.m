829903fb0b737922e0ab22592314162bc734403c
Eric <tungoo7@cs.utexas.edu>
2015-03-22 12:32:50 -0500

    Initial Commit

---
+++ BevoMaps/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BevoMaps
+//
+//  Created by Eric Nguyen on 3/22/15.
+//  Copyright (c) 2015 BevoMaps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
