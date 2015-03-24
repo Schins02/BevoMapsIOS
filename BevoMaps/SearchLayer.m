63b0f3c283cae138bfa2127f8b2b2a122ce90c9b
Trent Kan <trentkan@Trents-MacBook-Pro.local>
2015-03-24 18:00:08 -0500

    Basic functionality working, need to fine tune the parsing for the floor

--- BevoMaps/SearchLayer.m
+++ BevoMaps/SearchLayer.m
@@ -16,13 +16,52 @@
     return @{@"building" : @"GDC", @"floor" : [NSNull null]};
 }
 
+// Cases to handle later
 // NSString *input = @"GDC floor 1";
 // NSString *input = @"GDC first floor";
 
+// Correct input GDC 1.xxx
+// Assume correct input
 +(NSDictionary *)parseInputText:(NSString *)input
 {
+    assert (![input isEqualToString:@""]);
+    assert (input != nil);
     NSMutableDictionary *result = [[NSMutableDictionary alloc] init];
-    return result;
+    
+    @try
+    {
+        NSArray *searchResults = [input componentsSeparatedByString:@" "];
+        assert (searchResults.count == 2);
+        // Add building -- need to move to own method to error check
+        [result setObject:searchResults[0] forKey:[SearchLayer getKey:0]];
+        // Add floor -- need to move to own method to error check
+        [result setObject:searchResults[1] forKey:[SearchLayer getKey:1]];
+    }
+    @catch (NSException *e)
+    {
+        NSLog(@"Bad search term");
+    }
+    
+    return [NSDictionary dictionaryWithDictionary:result];
 }
+
+/******************/
+/******************/
+/* HELPER METHODS */
+/******************/
+/******************/
+
+// Returns the key given an index or an empty string if the index is out of bounds
++(NSString *)getKey:(NSInteger)forIndex
+{
+    NSArray *keys = @[@"building", @"floor"];
+    if (forIndex > keys.count - 1)
+    {
+        NSLog(@"Index requested out of range");
+        return @"";
+    }
+    return keys[forIndex];
+}
+
 
 @end
--- BevoMaps/ViewController.m
+++ BevoMaps/ViewController.m
@@ -7,16 +7,33 @@
 //
 
 #import "ViewController.h"
+#import "SearchLayer.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 
 @end
 
 @implementation ViewController
 
+-(void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
+{
+    [self handleSearch:searchBar];
+}
+
+- (void)handleSearch:(UISearchBar *)searchBar
+{
+    NSLog(@"User searched for %@", searchBar.text);
+    NSDictionary *searchResults = [SearchLayer parseInputText:searchBar.text];
+    NSLog(@"Parsed as: %@", searchResults);
+    [searchBar resignFirstResponder]; // if you want the keyboard to go away
+}
+
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+    self.searchBar.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
