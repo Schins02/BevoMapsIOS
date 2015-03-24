ffa354e4370f3d7cbf2ea26fad947fea163de176
Trent Kan <trentkan@Trents-MacBook-Pro.local>
2015-03-23 22:39:17 -0500

    Adding empty search layer

---
+++ BevoMaps/SearchLayer.h
@@ -0,0 +1,13 @@
+//
+//  SearchLayer.h
+//  BevoMaps
+//
+//  Created by Trent Kan on 3/23/15.
+//  Copyright (c) 2015 BevoMaps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SearchLayer : NSObject
+
+@end
---
+++ BevoMaps/SearchLayer.m
@@ -0,0 +1,13 @@
+//
+//  SearchLayer.m
+//  BevoMaps
+//
+//  Created by Trent Kan on 3/23/15.
+//  Copyright (c) 2015 BevoMaps. All rights reserved.
+//
+
+#import "SearchLayer.h"
+
+@implementation SearchLayer
+
+@end
