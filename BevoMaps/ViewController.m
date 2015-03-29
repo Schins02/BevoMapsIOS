8d658d11a67772a56385a83c30c0afe6bc3f99f0
Trent Kan <trentkan@wireless-10-147-146-83.public.utexas.edu>
2015-03-29 14:11:51 -0500

    adding floorplan view

--- BevoMaps/ViewController.m
+++ BevoMaps/ViewController.m
@@ -9,9 +9,17 @@
 #import "ViewController.h"
 #import "SearchLayer.h"
 
-@interface ViewController ()
 
+
+@interface ViewController () <UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+
+@property (strong, nonatomic) UIImageView *imageView;
+@property (strong, nonatomic) UIImage *image;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UILabel *currentFloor;
+
+@property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *swipeGesture;
 
 @end
 
@@ -27,18 +35,59 @@
     NSLog(@"User searched for %@", searchBar.text);
     NSDictionary *searchResults = [SearchLayer parseInputText:searchBar.text];
     NSLog(@"Parsed as: %@", searchResults);
-    [searchBar resignFirstResponder]; // if you want the keyboard to go away
+//    [searchBar resignFirstResponder]; // if you want the keyboard to go away
 }
 
-- (void)viewDidLoad {
-  [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+-(void)setScrollView:(UIScrollView *)scrollView
+{
+    _scrollView = scrollView;
+    _scrollView.minimumZoomScale = 0.25;
+    _scrollView.maximumZoomScale = 2.0;
+    _scrollView.delegate = self;
+    _scrollView.backgroundColor = [UIColor colorWithRed:232.0/255.0 green:221.0/255.0 blue:189.0/255.0 alpha:1.0];
+
+    self.scrollView.contentSize = self.image ? self.image.size : CGSizeZero;
+}
+
+-(UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.imageView;
+}
+
+-(UIImageView *)imageView
+{
+    if(!_imageView)
+    {
+        _imageView = [[UIImageView alloc] init];
+    }
+    return _imageView;
+}
+
+-(UIImage *)image
+{
+    return self.imageView.image;
+}
+
+-(void)setImage:(UIImage *)i
+{
+    self.imageView.image = i;
+    self.imageView.frame = CGRectMake(0, 0, self.imageView.image.size.width, self.imageView.image.size.height);
+    [self.imageView sizeToFit];
+    self.scrollView.contentSize = self.image ? self.image.size : CGSizeZero;
+    self.scrollView.zoomScale = .25;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.image = [UIImage imageNamed:@"GDCFloor1"];
+    [self.scrollView addSubview:self.imageView];
     self.searchBar.delegate = self;
+    self.scrollView.panGestureRecognizer.maximumNumberOfTouches = 1;
 }
 
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
-}
+
+
+
 
 @end
