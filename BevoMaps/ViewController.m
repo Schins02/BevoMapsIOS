c9fb76a1f6c89d5b6148f29972bdd613a8e85165
John Schindler <john.schindler@utexas.edu>
2015-04-02 11:56:15 -0500

    adding building view and swipe up functionality

--- BevoMaps/ViewController.m
+++ BevoMaps/ViewController.m
@@ -12,14 +12,13 @@
 
 
 @interface ViewController () <UIScrollViewDelegate>
-@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 @property (strong, nonatomic) UIImageView *imageView;
 @property (strong, nonatomic) UIImage *image;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-@property (weak, nonatomic) IBOutlet UILabel *currentFloor;
-
 @property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *swipeGesture;
+@property (nonatomic) unsigned floor;
 
 @end
 
@@ -35,17 +34,17 @@
     NSLog(@"User searched for %@", searchBar.text);
     NSDictionary *searchResults = [SearchLayer parseInputText:searchBar.text];
     NSLog(@"Parsed as: %@", searchResults);
-//    [searchBar resignFirstResponder]; // if you want the keyboard to go away
+    [searchBar resignFirstResponder]; // if you want the keyboard to go away
 }
 
 -(void)setScrollView:(UIScrollView *)scrollView
 {
     _scrollView = scrollView;
-    _scrollView.minimumZoomScale = 0.25;
+    _scrollView.minimumZoomScale = 0.1;
     _scrollView.maximumZoomScale = 2.0;
     _scrollView.delegate = self;
     _scrollView.backgroundColor = [UIColor colorWithRed:232.0/255.0 green:221.0/255.0 blue:189.0/255.0 alpha:1.0];
-
+    _scrollView.showsHorizontalScrollIndicator = YES;
     self.scrollView.contentSize = self.image ? self.image.size : CGSizeZero;
 }
 
@@ -74,7 +73,7 @@
     self.imageView.frame = CGRectMake(0, 0, self.imageView.image.size.width, self.imageView.image.size.height);
     [self.imageView sizeToFit];
     self.scrollView.contentSize = self.image ? self.image.size : CGSizeZero;
-    self.scrollView.zoomScale = .25;
+    self.scrollView.zoomScale = .14;
 }
 
 - (void)viewDidLoad
@@ -83,10 +82,28 @@
     self.image = [UIImage imageNamed:@"GDCFloor1"];
     [self.scrollView addSubview:self.imageView];
     self.searchBar.delegate = self;
+    self.floor = 1;
+    self.swipeGesture.numberOfTouchesRequired = 2;
+    self.swipeGesture.direction = UISwipeGestureRecognizerDirectionUp;
     self.scrollView.panGestureRecognizer.maximumNumberOfTouches = 1;
+    [self.scrollView.panGestureRecognizer requireGestureRecognizerToFail:self.swipeGesture];
 }
 
-
+- (IBAction)floorSwipe:(UISwipeGestureRecognizer *)sender {
+    
+    self.floor++;
+    if (self.floor == 8)
+    {
+        self.floor = 7;
+    }
+    else
+    {
+        NSString *current_floor = [NSString stringWithFormat:@"GDCFloor%u", self.floor];
+        self.scrollView.zoomScale = 1.0;
+        self.image = [UIImage imageNamed:current_floor];
+    }
+    
+}
 
 
 
