29af870a0db7567da03c468636ea67b40f78d41e
Schins02 <john.schindler@utexas.edu>
2015-04-07 01:55:32 -0500

    Update AppDelegate.m

--- BevoMaps/AppDelegate.m
+++ BevoMaps/AppDelegate.m
@@ -21,10 +21,7 @@
     
     [Parse setApplicationId:@"7Z6Ys0bNVFV95NuisNLS8gUd7WGLEIR7AM1kWuWR" clientKey:@"yYQdBeF8XSQgvpYZBnwDASbEdrxVXXO946QL4iaZ"];
     PFACL *defaultACL = [PFACL ACL];
-    
-    // If you would like all objects to be private by default, remove this line.
     [defaultACL setPublicReadAccess:YES];
-    
     [PFACL setDefaultACL:defaultACL withAccessForCurrentUser:YES];
     
     return YES;
